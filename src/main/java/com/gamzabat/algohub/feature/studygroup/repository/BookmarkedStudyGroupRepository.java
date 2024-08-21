2825b82d51be520bcc11a3a0e245c9fd326abc84
rladmstn <oz33177351@gmail.com>
2024-08-21 14:58:12 +0900

    feat : 스터디 그룹 즐겨찾기 추가/취소 API 추가

---
+++ src/main/java/com/gamzabat/algohub/feature/studygroup/repository/BookmarkedStudyGroupRepository.java
@@ -0,0 +1,13 @@
+package com.gamzabat.algohub.feature.studygroup.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gamzabat.algohub.feature.studygroup.domain.BookmarkedStudyGroup;
+import com.gamzabat.algohub.feature.studygroup.domain.StudyGroup;
+import com.gamzabat.algohub.feature.user.domain.User;
+
+public interface BookmarkedStudyGroupRepository extends JpaRepository<BookmarkedStudyGroup, Long> {
+	Optional<BookmarkedStudyGroup> findByUserAndStudyGroup(User user, StudyGroup studyGroup);
+}
