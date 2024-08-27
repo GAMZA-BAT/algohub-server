8809b9798d89948614bc078ff5fd940c3cf387ec
rladmstn <88280787+rladmstn@users.noreply.github.com>
2024-08-27 23:06:20 +0900

    refactor : URL 파일 분리  (#82)
    * feat : URL 상수 클래스 생성
    
    * refactor: 백준 닉네임 유효성 검증 API에서 URL ApiConstants 사용으로 수정
    
    * refactor: 문제 생성 API에서 URL ApiConstants 사용으로 수정
    
    * refactor: ApiConstants 생성자에 RuntimeException 추가

---
+++ src/main/java/com/gamzabat/algohub/common/ApiConstants.java
@@ -0,0 +1,10 @@
+package com.gamzabat.algohub.common;
+
+public final class ApiConstants {
+	public static final String SOLVED_AC_PROBLEM_API_URL = "https://solved.ac/api/v3/problem/lookup?problemIds=";
+	public static final String BOJ_USER_PROFILE_URL = "https://www.acmicpc.net/user/";
+
+	private ApiConstants() {
+		throw new RuntimeException("Can not instantiate : ApiConstants");
+	}
+}
