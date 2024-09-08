c92658258e6320f0dce6315149ac3a207c801222
rladmstn <oz33177351@gmail.com>
2024-09-08 16:55:10 +0900

    test : 문제 기간 수정 request 오늘 이전의 날짜를 요청한 경우 예외 service 테스트 작성

--- src/test/java/com/gamzabat/algohub/service/ProblemServiceTest.java
+++ src/test/java/com/gamzabat/algohub/service/ProblemServiceTest.java
@@ -427,6 +427,54 @@ class ProblemServiceTest {
 	}
 
 	@Test
+	@DisplayName("문제 정보 수정 실패 : 문제 시작 날짜를 오늘 이전의 날짜로 요청한 경우")
+	void editProblemFailed_6() {
+		// given
+		Problem problem = Problem.builder()
+			.studyGroup(group)
+			.link("link")
+			.startDate(LocalDate.now())
+			.endDate(LocalDate.now().plusDays(10))
+			.build();
+		EditProblemRequest request = EditProblemRequest.builder()
+			.problemId(20L)
+			.startDate(LocalDate.now().minusDays(3))
+			.endDate(LocalDate.now().plusDays(7))
+			.build();
+		when(problemRepository.findById(20L)).thenReturn(Optional.ofNullable(problem));
+		when(groupRepository.findById(10L)).thenReturn(Optional.ofNullable(group));
+		// when, then
+		assertThatThrownBy(() -> problemService.editProblem(user, request))
+			.isInstanceOf(ProblemValidationException.class)
+			.hasFieldOrPropertyWithValue("code", HttpStatus.BAD_REQUEST.value())
+			.hasFieldOrPropertyWithValue("error", "문제 시작 날짜는 오늘 이전의 날짜로 수정할 수 없습니다.");
+	}
+
+	@Test
+	@DisplayName("문제 정보 수정 실패 : 문제 마감 날짜를 오늘 이전의 날짜로 요청한 경우")
+	void editProblemFailed_7() {
+		// given
+		Problem problem = Problem.builder()
+			.studyGroup(group)
+			.link("link")
+			.startDate(LocalDate.now().minusDays(10))
+			.endDate(LocalDate.now())
+			.build();
+		EditProblemRequest request = EditProblemRequest.builder()
+			.problemId(20L)
+			.startDate(LocalDate.now().minusDays(10))
+			.endDate(LocalDate.now().minusDays(2))
+			.build();
+		when(problemRepository.findById(20L)).thenReturn(Optional.ofNullable(problem));
+		when(groupRepository.findById(10L)).thenReturn(Optional.ofNullable(group));
+		// when, then
+		assertThatThrownBy(() -> problemService.editProblem(user, request))
+			.isInstanceOf(ProblemValidationException.class)
+			.hasFieldOrPropertyWithValue("code", HttpStatus.BAD_REQUEST.value())
+			.hasFieldOrPropertyWithValue("error", "문제 마감 날짜는 오늘 이전의 날짜로 수정할 수 없습니다.");
+	}
+
+	@Test
 	@DisplayName("문제 목록 조회 성공")
 	void getProblemList() throws NoSuchFieldException, IllegalAccessException {
 		// given
