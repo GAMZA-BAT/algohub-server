ae19f0454e66697fcc7bf51213db7fc8981a48df
rladmstn <oz33177351@gmail.com>
2024-10-07 23:34:01 +0900

    feat : 랭킹 정보 반영할 Rank 엔티티 추가

---
+++ src/main/java/com/gamzabat/algohub/feature/studygroup/domain/Rank.java
@@ -0,0 +1,37 @@
+package com.gamzabat.algohub.feature.studygroup.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Rank {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private GroupMember member;
+
+	private int solvedCount;
+	private int rank;
+	private int rankDiff;
+
+	@Builder
+	public Rank(GroupMember member, int solvedCount, int rank, int rankDiff) {
+		this.member = member;
+		this.solvedCount = solvedCount;
+		this.rank = rank;
+		this.rankDiff = rankDiff;
+	}
+}
