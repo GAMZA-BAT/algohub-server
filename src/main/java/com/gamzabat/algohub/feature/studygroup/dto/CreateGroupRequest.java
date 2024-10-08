e6f25662d6107c28d2e1c5210471b8fb61fb56f5
sh0723 <90740057+sh0723@users.noreply.github.com>
2024-10-08 21:48:52 +0900

    refactor : 그룹이름 글자수 제한 (#116)
    * refactor : 그룹이름 글자수 제한
    
    * refactor : 그룹이름 글자수 제한 // 피드백 반영

--- src/main/java/com/gamzabat/algohub/feature/studygroup/dto/CreateGroupRequest.java
+++ src/main/java/com/gamzabat/algohub/feature/studygroup/dto/CreateGroupRequest.java
@@ -1,13 +1,13 @@
 package com.gamzabat.algohub.feature.studygroup.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record CreateGroupRequest(@NotBlank(message = "스터디 이름은 필수 입력 입니다.") String name,
-								 @NotNull(message = "스터디 시작 날짜는 필수 입력 입니다.") LocalDate startDate,
-								 @NotNull(message = "스터디 종료 날짜는 필수 입력 입니다.") LocalDate endDate,
-								 String introduction
+public record CreateGroupRequest(@Size(min = 1, max = 15, message = "스터디 이름은 1글자 이상 15글자 이하로 작성해야 합니다.") String name,
+                                 @NotNull(message = "스터디 시작 날짜는 필수 입력 입니다.") LocalDate startDate,
+                                 @NotNull(message = "스터디 종료 날짜는 필수 입력 입니다.") LocalDate endDate,
+                                 String introduction
 ) {
 }
