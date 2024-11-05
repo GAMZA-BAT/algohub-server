6204added99edd06a8a021392d06368f5f6ad993
rladmstn <oz33177351@gmail.com>
2024-11-05 23:53:48 +0900

    refactor : isVisible 타입 수정
    - boolean -> Boolean 수정
    - NotNull 추가

--- src/main/java/com/gamzabat/algohub/feature/group/studygroup/domain/GroupMember.java
+++ src/main/java/com/gamzabat/algohub/feature/group/studygroup/domain/GroupMember.java
@@ -14,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,7 +35,8 @@ public class GroupMember {
 	private LocalDate joinDate;
 	@Enumerated(EnumType.STRING)
 	private RoleOfGroupMember role;
-	private boolean isVisible;
+	@NotNull
+	private Boolean isVisible;
 
 	@Builder
 	public GroupMember(User user, StudyGroup studyGroup, LocalDate joinDate, RoleOfGroupMember role) {
