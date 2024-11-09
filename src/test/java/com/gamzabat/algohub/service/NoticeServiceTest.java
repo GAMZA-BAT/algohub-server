b29749e4ee2ee98bb86d4496c881f916f4447969
s-hwan <tmdghks507@daum.net>
2024-11-09 21:13:31 +0900

    ??

--- src/test/java/com/gamzabat/algohub/service/NoticeServiceTest.java
+++ src/test/java/com/gamzabat/algohub/service/NoticeServiceTest.java
@@ -181,7 +181,7 @@ public class NoticeServiceTest {
 		assertThat(response.content()).isEqualTo("content");
 		assertThat(response.title()).isEqualTo("title");
 		assertThat(response.category()).isEqualTo("category");
-		assertThat(response.createAt()).isEqualTo(DateFormatUtil.formatDate(LocalDateTime.now().toLocalDate()));
+		assertThat(response.createAt()).isEqualTo(DateFormatUtil.formatDateTimeForNotice(notice.getCreatedAt()));
 		assertThat(response.noticeId()).isEqualTo(1000L);
 	}
 
