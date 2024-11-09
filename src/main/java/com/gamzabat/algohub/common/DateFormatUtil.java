ad0361e05f01c9ff069fb1e1823d3933c49365d5
s-hwan <tmdghks507@daum.net>
2024-11-09 21:08:47 +0900

    why run failed...

--- src/main/java/com/gamzabat/algohub/common/DateFormatUtil.java
+++ src/main/java/com/gamzabat/algohub/common/DateFormatUtil.java
@@ -16,7 +16,7 @@ public final class DateFormatUtil {
 	}
 
 	public static String formatDateTimeForNotice(LocalDateTime dateTime) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy/MM.dd HH:mm");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy/MM/dd HH:mm");
 		return dateTime.format(formatter);
 	}
 }
