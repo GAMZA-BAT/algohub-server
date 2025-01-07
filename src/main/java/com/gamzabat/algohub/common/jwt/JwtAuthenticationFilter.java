b08d432466525e457bbbd719e9a421af43dcbf5f
rladmstn <oz33177351@gmail.com>
2025-01-07 16:10:49 +0900

    refactor : 불필요한 shouldNotFilter 호출 제거

--- src/main/java/com/gamzabat/algohub/common/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/gamzabat/algohub/common/jwt/JwtAuthenticationFilter.java
@@ -50,10 +50,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
-		if (shouldNotFilter(request)) {
-			filterChain.doFilter(request, response);
-			return;
-		}
 		try {
 			String token = tokenProvider.resolveToken(request);
 			if (token != null && tokenProvider.validateToken(token)) {
