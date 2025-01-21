cf2252ef794728a17b09266febe57c55a45982dc
s-hwan <tmdghks507@daum.net>
2025-01-21 23:10:54 +0900

    refactor: test를 잘 확인하자

--- src/test/java/com/gamzabat/algohub/service/StudyGroupServiceTest.java
+++ src/test/java/com/gamzabat/algohub/service/StudyGroupServiceTest.java
@@ -428,7 +428,7 @@ class StudyGroupServiceTest {
 	void editGroup() {
 		// given
 		EditGroupRequest request = new EditGroupRequest("editName", LocalDate.now().plusDays(10),
-			LocalDate.now().plusDays(10), "editIntroduction", false);
+			LocalDate.now().plusDays(10), "editIntroduction", true);
 		MockMultipartFile editImage = new MockMultipartFile("editImage", new byte[] {1, 2, 3});
 		when(studyGroupRepository.findById(anyLong())).thenReturn(Optional.ofNullable(group));
 		when(groupMemberRepository.findByUserAndStudyGroup(user, group)).thenReturn(Optional.ofNullable(groupMember1));
