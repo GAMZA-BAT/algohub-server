8fe352ffc54b324cf06dd2e541966d9081b707e0
s-hwan <tmdghks507@daum.net>
2025-01-26 23:19:50 +0900

    refactor: 뭔가... 큰 일이 일어나고 있는것만 같아요..!

--- src/main/java/com/gamzabat/algohub/feature/group/studygroup/dto/EditGroupRequest.java
+++ src/main/java/com/gamzabat/algohub/feature/group/studygroup/dto/EditGroupRequest.java
@@ -10,6 +10,6 @@ public record EditGroupRequest(
 	@Size(min = 1, max = 15, message = "스터디 이름은 1글자 이상 15글자 이하로 작성해야 합니다.") @Nullable String name,
 	LocalDate startDate,
 	LocalDate endDate,
-	String introduction,
+	@Nullable String introduction,
 	@NotNull(message = "기본 이미지 여부는 필수 입니다") Boolean isDefaultImage) {
 }
