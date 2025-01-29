b5cd4f50ba87c14f38e29c458a7827c496bbd9fd
s-hwan <tmdghks507@daum.net>
2025-01-29 22:06:57 +0900

    피드백 반영

--- src/main/java/com/gamzabat/algohub/feature/group/studygroup/dto/EditGroupRequest.java
+++ src/main/java/com/gamzabat/algohub/feature/group/studygroup/dto/EditGroupRequest.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record EditGroupRequest(
-	@Size(min = 1, max = 15, message = "스터디 이름은 1글자 이상 15글자 이하로 작성해야 합니다.") @Nullable String name,
+	@Size(min = 1, max = 15, message = "스터디 이름은 1글자 이상 15글자 이하로 작성해야 합니다.") String name,
 	LocalDate startDate,
 	LocalDate endDate,
 	@Nullable String introduction,
