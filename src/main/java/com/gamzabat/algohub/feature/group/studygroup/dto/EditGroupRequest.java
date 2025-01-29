f8a702e9706f69c8293b59c85895b5ec54eccd1a
s-hwan <tmdghks507@daum.net>
2025-01-29 22:58:45 +0900

    피드백 2차 반영

--- src/main/java/com/gamzabat/algohub/feature/group/studygroup/dto/EditGroupRequest.java
+++ src/main/java/com/gamzabat/algohub/feature/group/studygroup/dto/EditGroupRequest.java
@@ -2,14 +2,14 @@ package com.gamzabat.algohub.feature.group.studygroup.dto;
 
 import java.time.LocalDate;
 
-import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record EditGroupRequest(
-	@Size(min = 1, max = 15, message = "스터디 이름은 1글자 이상 15글자 이하로 작성해야 합니다.") String name,
+	@NotBlank @Size(min = 1, max = 15, message = "스터디 이름은 1글자 이상 15글자 이하로 작성해야 합니다.") String name,
 	LocalDate startDate,
 	LocalDate endDate,
-	@Nullable String introduction,
+	String introduction,
 	@NotNull(message = "기본 이미지 여부는 필수 입니다") Boolean isDefaultImage) {
 }
