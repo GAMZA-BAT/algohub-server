cc005c4762d5ddb48b06ed633c4fc67dccc7369c
s-hwan <tmdghks507@daum.net>
2025-02-26 18:27:03 +0900

    problem log 수정

--- src/main/java/com/gamzabat/algohub/feature/problem/service/ProblemService.java
+++ src/main/java/com/gamzabat/algohub/feature/problem/service/ProblemService.java
@@ -91,7 +91,7 @@ public class ProblemService {
 				NotificationCategory.PROBLEM_STARTED.getMessage(title)
 			);
 
-		log.info("success to create problem");
+		log.info("success to create problem user_id={}", user.getId());
 	}
 
 	@Transactional
@@ -118,7 +118,7 @@ public class ProblemService {
 			problem.editProblemEndDate(request.endDate());
 		}
 
-		log.info("success to edit problem deadline");
+		log.info("success to edit problem deadline user_id={}", user.getId());
 	}
 
 	private void checkProblemValidation(Problem problem) {
@@ -212,7 +212,7 @@ public class ProblemService {
 
 		solutionRepository.deleteAllByProblem(problem);
 		problemRepository.delete(problem);
-		log.info("success to delete problem");
+		log.info("success to delete problem user_id={}", user.getId());
 	}
 
 	@Transactional(readOnly = true)
