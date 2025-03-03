28203505e8500d23b2474502751ac58ad7427680
sh0723 <pshpark12345@naver.com>
2025-03-03 16:45:10 +0900

    feat : 로그인 API // 테스트 코드 수정

--- src/main/java/com/gamzabat/algohub/config/SpringSecurityConfig.java
+++ src/main/java/com/gamzabat/algohub/config/SpringSecurityConfig.java
@@ -3,8 +3,6 @@ package com.gamzabat.algohub.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,7 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.gamzabat.algohub.common.jwt.CustomUserDetailsService;
 import com.gamzabat.algohub.common.jwt.JwtAuthenticationFilter;
 import com.gamzabat.algohub.common.jwt.TokenProvider;
 
@@ -27,7 +24,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SpringSecurityConfig {
 	private final TokenProvider tokenProvider;
-	private final CustomUserDetailsService customUserDetailsService;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
@@ -48,14 +44,6 @@ public class SpringSecurityConfig {
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
 		return configuration.getAuthenticationManager();
-	}
-
-	@Bean
-	public AuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setUserDetailsService(customUserDetailsService);
-		provider.setPasswordEncoder(passwordEncoder());
-		return provider;
 	}
 
 }
