7e5b3baab1025e517a6aecc06027f6b771221ae2
s-hwan <tmdghks507@daum.net>
2025-03-07 14:59:43 +0900

    refactor:dto 수정 NotNull->NotBlank

--- src/main/java/com/gamzabat/algohub/feature/user/dto/RegisterBjNickNameRequest.java
+++ src/main/java/com/gamzabat/algohub/feature/user/dto/RegisterBjNickNameRequest.java
@@ -1,6 +1,6 @@
 package com.gamzabat.algohub.feature.user.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
-public record RegisterBjNickNameRequest(@NotNull(message = "백준 아이디는 필수 입력입니다") String bjNickName) {
+public record RegisterBjNickNameRequest(@NotBlank(message = "백준 아이디는 필수 입력입니다") String bjNickName) {
 }
