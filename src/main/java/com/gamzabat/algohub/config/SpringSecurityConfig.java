30db261b1fec581880199c539dde75ab577a7218
sh0723 <pshpark12345@naver.com>
2025-03-07 15:18:24 +0900

    refactor : 로그인 API // PR반영

--- src/main/java/com/gamzabat/algohub/config/SpringSecurityConfig.java
+++ src/main/java/com/gamzabat/algohub/config/SpringSecurityConfig.java
@@ -2,8 +2,6 @@ package com.gamzabat.algohub.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -39,11 +37,6 @@ public class SpringSecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-		return configuration.getAuthenticationManager();
 	}
 
 }
