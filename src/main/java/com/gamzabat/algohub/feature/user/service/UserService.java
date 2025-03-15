efc55dd19eec59647013d093a0caec8508f6098a
s-hwan <tmdghks507@daum.net>
2025-03-15 15:40:18 +0900

    refactor: 반복 되는 코드 분리

--- src/main/java/com/gamzabat/algohub/feature/user/service/UserService.java
+++ src/main/java/com/gamzabat/algohub/feature/user/service/UserService.java
@@ -190,26 +190,7 @@ public class UserService {
 
 	@Transactional
 	public void registerBjNickname(User user, RegisterBjNickNameRequest request) {
-		String bjUserUrl = BOJ_USER_PROFILE_URL + request.bjNickName();
-
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("User-Agent",
-			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36");
-		HttpEntity<String> entity = new HttpEntity<>(headers);
-
-		try {
-			restTemplate.exchange(bjUserUrl, HttpMethod.GET, entity, String.class);
-			// TODO : 백준 본인 인증 관련 사항 확정 후 로직 수정
-			// if (userRepository.existsByBjNickname(bjNickname))
-			// 	throw new CheckBjNicknameValidationException(HttpStatus.CONFLICT.value(), "이미 가입된 백준 닉네임 입니다.");
-		} catch (HttpClientErrorException e) {
-			if (e.getStatusCode() == HttpStatus.NOT_FOUND)
-				throw new CheckBjNicknameValidationException(HttpStatus.NOT_FOUND.value(), "백준 닉네임이 유효하지 않습니다.");
-		} catch (HttpServerErrorException e) {
-			log.error("BOJ server error occurred : " + e.getMessage());
-			throw new BOJServerErrorException("현재 백준 서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
-		}
-
+		validateBjNickname(request.bjNickName());
 		user.editBjNickname(request.bjNickName());
 		userRepository.save(user);
 		log.info("success to register baekjoon-nickname user_id = {}", user.getId());
@@ -217,25 +198,7 @@ public class UserService {
 
 	@Transactional(readOnly = true)
 	public void checkBjNickname(String bjNickname) {
-		String bjUserUrl = BOJ_USER_PROFILE_URL + bjNickname;
-
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("User-Agent",
-			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36");
-		HttpEntity<String> entity = new HttpEntity<>(headers);
-
-		try {
-			restTemplate.exchange(bjUserUrl, HttpMethod.GET, entity, String.class);
-			// TODO : 백준 본인 인증 관련 사항 확정 후 로직 수정
-			// if (userRepository.existsByBjNickname(bjNickname))
-			// 	throw new CheckBjNicknameValidationException(HttpStatus.CONFLICT.value(), "이미 가입된 백준 닉네임 입니다.");
-		} catch (HttpClientErrorException e) {
-			if (e.getStatusCode() == HttpStatus.NOT_FOUND)
-				throw new CheckBjNicknameValidationException(HttpStatus.NOT_FOUND.value(), "백준 닉네임이 유효하지 않습니다.");
-		} catch (HttpServerErrorException e) {
-			log.error("BOJ server error occurred : " + e.getMessage());
-			throw new BOJServerErrorException("현재 백준 서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
-		}
+		validateBjNickname(bjNickname);
 		log.info("success to check baekjoon nickname validity nickname = {}", bjNickname);
 	}
 
@@ -364,6 +327,30 @@ public class UserService {
 		byte[] bytes = new byte[TOKEN_LENGTH];
 		new SecureRandom().nextBytes(bytes);
 		return Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
+	}
+
+	private void validateBjNickname(String bjNickname) {
+
+		String bjUserUrl = BOJ_USER_PROFILE_URL + bjNickname;
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("User-Agent",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36");
+		HttpEntity<String> entity = new HttpEntity<>(headers);
+
+		try {
+			restTemplate.exchange(bjUserUrl, HttpMethod.GET, entity, String.class);
+			// TODO : 백준 본인 인증 관련 사항 확정 후 로직 수정
+			// if (userRepository.existsByBjNickname(bjNickname))
+			// 	throw new CheckBjNicknameValidationException(HttpStatus.CONFLICT.value(), "이미 가입된 백준 닉네임 입니다.");
+		} catch (HttpClientErrorException e) {
+			if (e.getStatusCode() == HttpStatus.NOT_FOUND)
+				throw new CheckBjNicknameValidationException(HttpStatus.NOT_FOUND.value(), "백준 닉네임이 유효하지 않습니다.");
+		} catch (HttpServerErrorException e) {
+			log.error("BOJ server error occurred : " + e.getMessage());
+			throw new BOJServerErrorException("현재 백준 서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
+		}
+
 	}
 
 }
