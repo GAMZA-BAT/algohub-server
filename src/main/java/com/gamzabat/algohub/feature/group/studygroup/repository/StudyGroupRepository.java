75476d96be52e097d446be4f0919039438e2bf2c
s-hwan <tmdghks507@daum.net>
2025-08-17 20:44:53 +0900

    feat:스터디 그룹 검색 API

--- src/main/java/com/gamzabat/algohub/feature/group/studygroup/repository/StudyGroupRepository.java
+++ src/main/java/com/gamzabat/algohub/feature/group/studygroup/repository/StudyGroupRepository.java
@@ -2,8 +2,11 @@ package com.gamzabat.algohub.feature.group.studygroup.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.gamzabat.algohub.feature.group.studygroup.domain.StudyGroup;
 import com.gamzabat.algohub.feature.group.studygroup.repository.querydsl.CustomStudyGroupRepository;
@@ -14,4 +17,8 @@ public interface StudyGroupRepository extends JpaRepository<StudyGroup, Long>, C
 
 	@Query("select sg from StudyGroup sg where sg.id = :id and sg.deletedAt is null")
 	Optional<StudyGroup> findById(Long id);
+
+	@Query("select sg from StudyGroup sg where lower(sg.name) like(concat('%', lower(:searchPattern),'%'))"
+		+ "or lower(sg.introduction) like(concat('%',lower(:searchPattern),'%'))")
+	Page<StudyGroup> findBySearchPattern(@Param("searchPattern") String searchPattern, Pageable pageable);
 }
--- src/main/java/com/gamzabat/algohub/feature/group/studygroup/service/StudyGroupService.java
+++ src/main/java/com/gamzabat/algohub/feature/group/studygroup/service/StudyGroupService.java
@@ -11,6 +11,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -90,6 +92,19 @@ public class StudyGroupService {
 	private final NotificationSettingRepository notificationSettingRepository;
 	private final NotificationService notificationService;
 	private final DiscordWebhookService webhookService;
+
+	private static boolean isDone(LocalDate today, StudyGroup group) {
+		return group.getEndDate() != null && group.getEndDate().isBefore(today);
+	}
+
+	private static boolean isInProgress(LocalDate today, StudyGroup group) {
+		return !(group.getStartDate() == null || group.getStartDate().isAfter(today))
+			&& !(group.getEndDate() == null || group.getEndDate().isBefore(today));
+	}
+
+	private static boolean isQueued(LocalDate today, StudyGroup group) {
+		return group.getStartDate() != null && group.getStartDate().isAfter(today);
+	}
 
 	@Transactional
 	public GroupCodeResponse createGroup(User user, CreateGroupRequest request, MultipartFile profileImage) {
@@ -291,19 +306,6 @@ public class StudyGroupService {
 
 		log.info("success to get my study group list");
 		return response;
-	}
-
-	private static boolean isDone(LocalDate today, StudyGroup group) {
-		return group.getEndDate() != null && group.getEndDate().isBefore(today);
-	}
-
-	private static boolean isInProgress(LocalDate today, StudyGroup group) {
-		return !(group.getStartDate() == null || group.getStartDate().isAfter(today))
-			&& !(group.getEndDate() == null || group.getEndDate().isBefore(today));
-	}
-
-	private static boolean isQueued(LocalDate today, StudyGroup group) {
-		return group.getStartDate() != null && group.getStartDate().isAfter(today);
 	}
 
 	private GetStudyGroupResponse getStudyGroupResponseDTO(User user, StudyGroup group) {
@@ -634,4 +636,12 @@ public class StudyGroupService {
 		log.info("success to get my study groups settings");
 		return response;
 	}
+
+	@Transactional(readOnly = true)
+	public Page<GetGroupResponse> getSearchedStudyGroupList(String searchPattern, Pageable pageable) {
+		Page<StudyGroup> groups = groupRepository.findBySearchPattern(searchPattern, pageable);
+		return groups.map(group -> new GetGroupResponse(group.getId(), group.getName(), group.getStartDate(),
+			group.getEndDate(), group.getIntroduction(), group.getGroupImage(), null, null));
+	}
+
 }
