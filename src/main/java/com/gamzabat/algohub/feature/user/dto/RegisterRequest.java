a6f4c259e4370a2052e265512cba868d21e8faef
hwangjokim <hg7258@naver.com>
2025-08-24 21:50:47 +0900

    feat : Require bjNickname field when signing up

--- src/main/java/com/gamzabat/algohub/feature/user/dto/RegisterRequest.java
+++ src/main/java/com/gamzabat/algohub/feature/user/dto/RegisterRequest.java
@@ -3,5 +3,7 @@ package com.gamzabat.algohub.feature.user.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record RegisterRequest(@NotBlank(message = "비밀번호는 필수 입력입니다.") String password,
-							  @NotBlank(message = "닉네임은 필수 입력입니다.") String nickname){
+							  @NotBlank(message = "닉네임은 필수 입력입니다.") String nickname,
+							  @NotBlank(message = "백준 아이디는 필수 입력입니다.") String bjNickname
+) {
 }
