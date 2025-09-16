8880f4936937a64015559ab8d2bde2c00578fb6c
shshwn <87060472+s-hwan@users.noreply.github.com>
2025-09-16 14:51:20 +0900

    feat:스터디 그룹 검색 API (#391)

--- src/main/java/com/gamzabat/algohub/feature/group/studygroup/repository/StudyGroupRepository.java
+++ src/main/java/com/gamzabat/algohub/feature/group/studygroup/repository/StudyGroupRepository.java
@@ -2,8 +2,11 @@ package com.gamzabat.algohub.feature.group.studygroup.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.gamzabat.algohub.feature.group.studygroup.domain.StudyGroup;
 import com.gamzabat.algohub.feature.group.studygroup.repository.querydsl.CustomStudyGroupRepository;
@@ -14,4 +17,11 @@ public interface StudyGroupRepository extends JpaRepository<StudyGroup, Long>, C
 
 	@Query("select sg from StudyGroup sg where sg.id = :id and sg.deletedAt is null")
 	Optional<StudyGroup> findById(Long id);
+
+	@Query(value = """
+		select sg from StudyGroup sg
+		where match (sg.name,sg.introduction)
+		against(:searchPattern in natural language mode) 
+		""", nativeQuery = true)
+	Page<StudyGroup> findBySearchPattern(@Param("searchPattern") String searchPattern, Pageable pageable);
 }
