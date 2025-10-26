924106b2fb4546b9301cd47e774fab20850352db
sh0723 <pshpark12345@naver.com>
2025-10-26 21:32:15 +0900

    사용되지 않는 user파라미터 제거

--- src/main/java/com/gamzabat/algohub/feature/edgecase/controller/EdgeCaseController.java
+++ src/main/java/com/gamzabat/algohub/feature/edgecase/controller/EdgeCaseController.java
@@ -49,7 +49,6 @@ public class EdgeCaseController {
 	@GetMapping("/list")
 	@Operation(summary = "반례리스트 조회")
 	public ResponseEntity<GetEdgeCaseListResponse> getEdgeCaseList(
-		@AuthedUser User user,
 		@RequestParam(required = false) Integer problemNumber,
 		@RequestParam(required = false, defaultValue = "RECENT") EdgeCaseSortType sort
 	) {
