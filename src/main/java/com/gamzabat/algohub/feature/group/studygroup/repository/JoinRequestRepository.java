e10e5a844b9acb1ae3181b025b5920fca75b1c60
shshwn <87060472+s-hwan@users.noreply.github.com>
2025-11-09 22:15:36 +0900

    refactor : 그룹 가입 요청 목록 조회  (#407)

--- src/main/java/com/gamzabat/algohub/feature/group/studygroup/repository/JoinRequestRepository.java
+++ src/main/java/com/gamzabat/algohub/feature/group/studygroup/repository/JoinRequestRepository.java
@@ -3,6 +3,8 @@ package com.gamzabat.algohub.feature.group.studygroup.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.gamzabat.algohub.feature.group.studygroup.domain.JoinRequest;
 
@@ -10,5 +12,13 @@ public interface JoinRequestRepository extends JpaRepository<JoinRequest, Long> 
 
 	boolean existsByGroup_IdAndRequester_Id(Long groupId, Long userId);
 
-	List<JoinRequest> findAllByGroup_Id(Long groupId);
+	@Query("""
+		select jr
+		from JoinRequest jr
+		join fetch jr.requester u
+		join fetch jr.group g
+		where g.id = :groupId
+		""")
+	List<JoinRequest> findAllByGroupIdWithFetch(@Param("groupId") Long groupId);
+
 }
